412e32632b9306dadb3044201e1f7e576818320c
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-19 20:36:13 +0000

    Updated readme

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # planner-app
 Planner/Scheduler App
+
+Displayed the current day at the top of the calender when a user opens the planner.
+
+Showcased timeblocks for standard business hours when the user scrolls down.
+
+Color-coded each timeblock based on past, present, and future timing for when the timeblock is viewed.
+
+// Allow a user to enter an event when they click a timeblock
+
+//Save the event in local storage when the save button is clicked in that timeblock.
+
+//Persist events between refreshes of a page
--- starter/script.js
+++ starter/script.js
@@ -4,17 +4,18 @@ var today = dayjs();
 currentDay.text(today.format("D MMM YYYY"));
 
 function timeBlock() {
-    var currentHour = dayjs().format("H");
-    console.log(currentHour);
+    var currentHour = 12;
+    
     $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id"));
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
         if (blockHour < currentHour) {
             $(this).addClass("past");
-        } else if (blockHour == currentHour) {
+        } else if (blockHour === currentHour) {
             $(this).addClass("present");
         } else {
             $(this).addClass("future");
         }
+        console.log(currentHour, blockHour);
     });
 }
 
