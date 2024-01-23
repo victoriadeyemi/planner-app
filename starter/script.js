6c226705f4e6cbb4b7a6470a9e8ac61e705bcd95
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-23 18:01:14 +0000

    corrrected if statement

--- starter/script.js
+++ starter/script.js
@@ -17,7 +17,7 @@ function timeBlock() {
         var blockHour = parseInt($(this).attr("id").split("-")[1]);
         if (blockHour < currentHour) {
             $(this).addClass("past");
-        } else if (blockHour === currentHour) {
+        } else if (blockHour == currentHour) {
             $(this).addClass("present");
         } else {
             $(this).addClass("future");
